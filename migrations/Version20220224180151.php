edb8b092d166d05d46a4bfbf6bac54f395397616
Victor Ya <yavictor37@yandex.ru>
2022-02-24 22:10:46 +0300

    Created Category. Added Product category to sonata admin

---
+++ migrations/Version20220224180151.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220224180151 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD727ACA70 FOREIGN KEY (parent_id) REFERENCES product (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE messenger_messages CHANGE body body LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE headers headers LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE queue_name queue_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD727ACA70');
+        $this->addSql('ALTER TABLE product CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+//use App\Entity\Category;
+//use Doctrine\ORM\EntityManager;
+//use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+
+    /**
+     * @throws \Exception
+     */
+    public function homepage(Request $request): Response
+    {
+        $random = random_int(0, 100);
+
+        $name = $request->query->get('name', 'undefinedName');
+
+        return $this->render('default/index.html.twig', [
+            'name' => $name,
+            'random' => $random,
+        ]);
+    }
+//    public function homepage(Request $request, EntityManagerInterface $em): Response
+//    {
+//        $cr = $em->getRepository('App:Category');
+//        $categories = $cr->findAll();
+//
+//        return $this->render('default/index.html.twig', [
+//            'categories' => $categories,
+//        ]);
+//    }
+//
+//    public function category(int $id, EntityManagerInterface $em)
+//    {
+//        $category = $em->getRepository('App:Category')->find($id);
+//
+//        return $this->render('default/category.html.twig', [
+//            'category' => $category,
+//        ]);
+//    }
+}
